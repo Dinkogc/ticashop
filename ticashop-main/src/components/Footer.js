1ce7b597b21a743678fffe9f63fa53318a585d74
Dinkogc <dinko.guerra@inacapmail.cl>
2025-10-07 21:05:11 -0300

    Update Footer.js

--- ticashop-main/src/components/Footer.js
+++ ticashop-main/src/components/Footer.js
@@ -1,11 +1,12 @@
-import React from "react";
+noimport React from "react";
 
 function Footer() {
   return (
     <footer>
-      <p>© 2025 Mi Tienda - Todos los derechos reservados</p>
+      <p>© 2025 TICASHOP LATAM - Todos los derechos reservados</p>
     </footer>
   );
 }
+
 
 export default Footer;
